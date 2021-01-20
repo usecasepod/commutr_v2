12b4532ca4e0feb75f84df0810e6776c9e062e6a
Austin Webre <austin.webre@gmail.com>
2021-01-19 21:58:13 -0600

    WIP fill up listing

--- commutr_v2/Views/FillUpListing.fs
+++ commutr_v2/Views/FillUpListing.fs
@@ -1,15 +1,20 @@
 namespace CommutrV2.Views
 
+open CommutrV2
+open CommutrV2.Components
 open CommutrV2.Models.FillUps
+open CommutrV2.Models.Vehicles
 open CommutrV2.Models
 open CommutrV2.Data.FillUpRepository
 open CommutrV2.Utils.Loading
 open Fabulous
+open Fabulous.XamarinForms
+open Xamarin.Forms
+
 
 module FillUpListing =
-
     type Model =
-        { VehicleId: VehicleId.T
+        { Vehicle: Vehicle
           FillUpLoadable: FillUp Loadable }
 
     type ExternalMsg =
@@ -46,8 +51,8 @@ module FillUpListing =
             return msg
         }
 
-    let init (vehicleId) =
-        { VehicleId = vehicleId
+    let init (vehicle) =
+        { Vehicle = vehicle
           FillUpLoadable = Loading },
         Cmd.ofMsg LoadFillUps
 
@@ -55,7 +60,7 @@ module FillUpListing =
         match msg with
         | LoadFillUps ->
             let cmd =
-                Cmd.ofAsyncMsg (loadAsync (model.VehicleId))
+                Cmd.ofAsyncMsg (loadAsync (model.Vehicle.Id))
 
             let m = { model with FillUpLoadable = Loading }
             m, cmd, ExternalMsg.NoOp
@@ -66,14 +71,81 @@ module FillUpListing =
 
             m, Cmd.none, ExternalMsg.NoOp
 
-// let view model dispatch =
-//      let content = match model.FillUpLoadable with
-//                    | Loading ->
-//                         View.Label
-//                             (text = "Loading...",
-//                              horizontalTextAlignment = TextAlignment.Center,
-//                              verticalTextAlignment = TextAlignment.Center,
-//                              fontAttributes = FontAttributes.Bold,
-//                              textColor = AppColors.cinereous)
-//                    | Loaded fillUps ->
-//                        let items = fillUps |>
+    let view model dispatch =
+        let content =
+            match model.FillUpLoadable with
+            | Loading ->
+                View.Label(
+                    text = "Loading...",
+                    horizontalTextAlignment = TextAlignment.Center,
+                    verticalTextAlignment = TextAlignment.Center,
+                    fontAttributes = FontAttributes.Bold,
+                    textColor = AppColors.cinereous
+                )
+            | Loaded fillUps ->
+                let emptyView =
+                    View.StackLayout(
+                        spacing = 5.0,
+                        backgroundColor = AppColors.silverSandLight,
+                        padding = Thickness 25.0,
+                        children =
+                            [ View.Label(
+                                text = "You don't have any fill ups recorded yet!",
+                                horizontalTextAlignment = TextAlignment.Center,
+                                verticalTextAlignment = TextAlignment.Center,
+                                fontAttributes = FontAttributes.Bold,
+                                textColor = AppColors.cinereous
+                              )
+                              View.Button(
+                                  text = "Record a Fill Up",
+                                  backgroundColor = AppColors.cinereous,
+                                  textColor = AppColors.ghostWhite,
+                                  command = fun () -> dispatch NewFillUpTapped
+                              ) ]
+                    )
+
+                let items =
+                    fillUps
+                    |> List.map
+                        (fun item ->
+                            View.SwipeView(
+                                backgroundColor = AppColors.silverSandLight,
+                                rightItems =
+                                    View.SwipeItems(
+                                        items =
+                                            [ View.SwipeItem(
+                                                text = "Delete",
+                                                backgroundColor = Color.Red,
+                                                command = fun () -> dispatch (RemoveFillUp item)
+                                              )
+                                              View.SwipeItem(
+                                                  text = "Edit",
+                                                  backgroundColor = AppColors.silverSandMediumDark,
+                                                  command = fun () -> dispatch (UpdateFillUp item)
+                                              ) ]
+                                    ),
+                                content = FillUpCell.view item
+                            ))
+
+                View.CollectionView(items, emptyView = emptyView, selectionMode = SelectionMode.Single)
+
+
+        View.ContentPage(
+            View.AbsoluteLayout(
+                [ content
+                  View
+                      .Button(text = "+",
+                              fontSize = FontSize.fromValue 24.0,
+                              backgroundColor = AppColors.mandarin,
+                              textColor = AppColors.ghostWhite,
+                              command = (fun () -> dispatch NewFillUpTapped),
+                              padding = Thickness 10.0)
+                      .WidthRequest(60.0)
+                      .HeightRequest(60.0)
+                      .ButtonCornerRadius(30)
+                      .LayoutFlags(AbsoluteLayoutFlags.PositionProportional)
+                      .LayoutBounds(Rectangle(0.90, 1.0, AbsoluteLayout.AutoSize, AbsoluteLayout.AutoSize)) ]
+            ),
+            backgroundColor = AppColors.silverSandLight,
+            title = formattedName model.Vehicle
+        )
